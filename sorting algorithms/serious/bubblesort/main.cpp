35d4751b18329cf7c69579e9b6662f8d429cfe27
Rudy <74006682+RudiG111@users.noreply.github.com>
2025-02-23 04:34:18 -0800

    Create main.cpp

---
+++ sorting algorithms/serious/bubblesort/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    long long nr=0;
+    short n;
+    cout<<"The number of the elements of the array:";
+    cin>>n;
+    vector <short>arr;
+    for(int i=1;i<=n;i++)
+    arr.push_back(i);
+    bool sorted=false;
+    random_shuffle(arr.begin(),arr.end());
+    for(int i: arr)
+      cout<<i<<" ";
+      cout<<endl;
+    while(!sorted)
+    {sorted=true;
+      for(int i=0;i<n-1;i++)
+      {if(arr[i]>arr[i+1]){ sorted=false;
+      swap(arr[i],arr[i+1]);
+        nr++;
+        }
+      }
+    }
+  for(int i: arr)
+      cout<<i<<" ";
+      cout<<"Number of comparisons:"<<nr<<endl;
+    return 0;
+}
